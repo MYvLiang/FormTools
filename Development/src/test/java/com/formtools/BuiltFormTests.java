5a4c2f1959b80bdfc1ac9f2fbd8854c5abf8c1b6
MYvLiang <13030235667@163.com>
2020-02-05 21:55:33 +0800

    修改类名BuiltFormTests

--- Development/src/test/java/com/formtools/BuiltFormTests.java
+++ Development/src/test/java/com/formtools/BuiltFormTests.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @SpringBootTest
-class FormtoolsApplicationTests {
+class BuiltFormTests {
 
     @Resource
     BuiltFormMapper builtFormMapper;
