86f5b33e487211d75daf5044d3ab359839c4e21f
MYvLiang <13030235667@163.com>
2020-02-05 13:55:10 +0800

    创建开发环境Development，使用的框架：springboot+mybatis

---
+++ Development/src/main/java/com/formtools/FormtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.formtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormtoolsApplication.class, args);
+    }
+
+}
---
+++ Development/src/test/java/com/formtools/FormtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormtoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
