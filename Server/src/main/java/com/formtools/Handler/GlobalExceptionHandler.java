948120dfc9aa1518d4746ed22bbcaca71669569b
NaNRaligun <nanrailgun@aliyun.com>
2020-02-29 21:49:20 +0800

    添加400&404统一处理

--- Server/src/main/java/com/formtools/Handler/GlobalExceptionHandler.java
+++ Server/src/main/java/com/formtools/Handler/GlobalExceptionHandler.java
@@ -6,10 +6,13 @@ import com.formtools.vo.ResultVo;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -38,4 +41,16 @@ public class GlobalExceptionHandler {
         return ResultVo.fail(ErrorMsg.PARAM_ERROR,e.getMap());
     }
 
+    @ExceptionHandler({MissingServletRequestParameterException.class,HttpRequestMethodNotSupportedException.class})
+    public Map MissingServletRequestParameterExceptionHandler(Exception e){
+        Map<String,Object> map=new LinkedHashMap<>();
+        map.put("status","400");
+        map.put("message",e.getMessage());
+        return map;
+    }
+
+    /*@ExceptionHandler(Exception.class)
+    public String CommonExceptionHandler(){
+        return "服务器错误";
+    }*/
 }
